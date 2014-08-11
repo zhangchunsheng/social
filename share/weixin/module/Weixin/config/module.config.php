1c0e51868eb065b464382c5b90177a680288f273
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-08-11 20:35:51 +0800

    1.新功能:增加微信应用

--- share/weixin/module/Weixin/config/module.config.php
+++ share/weixin/module/Weixin/config/module.config.php
@@ -49,7 +49,7 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'weixin/index/index' => __DIR__ . '/../view/weixin/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
