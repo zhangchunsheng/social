d6e5db7fcff29383dcbf064fae0e148b41db1ca2
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-11-17 20:55:26 +0800

    1.新功能:增加微信群发功能

---
+++ share/weixin_message/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @title index
+ * @description
+ * index
+ * @author zhangchunsheng423@gmail.com
+ * @version V1.0
+ * @date 2014-11-17
+ * @copyright  Copyright (c) 2010-2014 Luomor Inc. (http://www.luomor.com)
+ */
+include 'lanewechat/lanewechat.php';
+//获取自定义菜单列表
+$menuList = Menu::getMenu();
