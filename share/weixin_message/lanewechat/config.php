9ee0d6edaad9265f303c7f2e479a8e56797cf55c
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-11-17 21:01:20 +0800

    1.新功能:增加微信群发功能

--- share/weixin_message/lanewechat/config.php
+++ share/weixin_message/lanewechat/config.php
@@ -18,8 +18,8 @@ define("WECHAT_URL", 'http://www.lanecn.com');
 define('WECHAT_TOKEN', 'weixin');
 define('ENCODING_AES_KEY', "MqAuKoex6FyT5No0OcpRyCicThGs0P1vz4mJ2gwvvkF");
 
-define("WECHAT_APPID", 'wx5d57f64bb4804d90');
-define("WECHAT_APPSECRET", '4b1fa6d9442351ec9268eff05e38f521');
+define("WECHAT_APPID", 'wx5d57f64bb4804d90');// wx44d0e65f9951d33b wx5d57f64bb4804d90
+define("WECHAT_APPSECRET", '5f1c5968f3b4978932cac17492f8da71');// 4b1fa6d9442351ec9268eff05e38f521 5f1c5968f3b4978932cac17492f8da71
 
 
 ////-----引入系统所需类库-------------------
